d5f8025e3a3e8a86dd7b6aafd064b8628fc68fab
JacesChrist <derrick1rose95@gmail.com>
2020-05-19 18:47:34 +0200

    implemented delete button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,15 @@ export class AppComponent {
     students: Student[] = [
         new Student("123456", "studentLastName2", "studentFirstName2"),
         new Student("234567", "studentLastName2", "studentFirstName2"),
-        new Student("345678", "studentLastName3", "studentFirstName3")
+        new Student("345678", "studentLastName3", "studentFirstName3"),
+        new Student("456789", "studentLastName4", "studentFirstName4"),
+        new Student("567890", "studentLastName5", "studentFirstName5"),
     ];
-    displayedColumnsStudents: string[] = ["select", "id", "name", "firstName"];
+    displayedColumnsStudents: string[] = ["select", "delete", "id", "name", "firstName"];
 
     dataSource = new MatTableDataSource<Student>(this.students);
     selection = new SelectionModel<Student>(true, []);
+
     masterToggle() {
         this.isAllSelected() ?
             this.selection.clear() :
@@ -38,4 +41,15 @@ export class AppComponent {
         }
         return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
     }
+    
+    deleteSelected() {
+      let unselected: Student[] = [];
+      for(let student of this.students) {
+        if(!this.selection.isSelected(student)) {
+          unselected.push(student);
+        }
+      }
+      this.students = unselected;
+    }
+
 }
