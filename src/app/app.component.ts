13e426f1a22acd4a5d767fd4ab19c834d33659c2
JacesChrist <derrick1rose95@gmail.com>
2020-05-20 21:33:12 +0200

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -97,16 +97,13 @@ export class AppComponent {
     optionSelected(student: Student) {
         this.selectedStudent = student;
     }
-
-    compareStudents(student1: Student, student2: Student) {
-        if(student1.id > student2.id)
-            return 1;
-        if(student1.id < student2.id)
-            return -1;
-        return 0;
-    }
     
     addOption() {
+      this.filteredOptions = this.myControl.valueChanges
+        .pipe(
+          startWith(''),
+          map(value => this._filter(value))
+        );
       if(this.selectedStudent != null) {
         for(let student of this.students) {
           if(student.id === this.selectedStudent.id) {
@@ -115,9 +112,7 @@ export class AppComponent {
         }
         this.students.push(this.selectedStudent);
         this.selectedStudent = null;
-        this.students = this.students.sort(this.compareStudents);
         this.dataSource = new MatTableDataSource<Student>(this.students);
-        this.students = this.students.slice();
       }
     }
 }
